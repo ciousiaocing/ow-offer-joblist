f4b345cff503039a45ff086e53e77ff316d11358
ciou <yyouko22@gmail.com>
2017-05-18 11:54:47 +0800

    admin can see and download resumes(can't download) & fix filter order bug

--- app/controllers/admin/jobs_controller.rb
+++ app/controllers/admin/jobs_controller.rb
@@ -4,14 +4,7 @@ class Admin::JobsController < ApplicationController
   layout "admin"
 
   def index
-    @jobs = case params[:order]
-    when 'by_lower_bound'
-      Job.published.order('wage_lower_bound DESC')
-    when 'by_upper_bound'
-      Job.published.order('wage_upper_bound DESC')
-    else
-      Job.publish.recent
-    end
+    @jobs = Job.all
   end
 
   def show
